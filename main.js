0cb7616ef10998881793c8d331cdceebdf507bd5
Dim <fitform163@mail.ru>
2025-05-20 19:02:27 +0300

    pravka

--- main.js
+++ main.js
@@ -1,37 +1,53 @@
 // Задание № 1 код с помощью условной конструкции по блок схеме
 
-let A = Number(prompt("введите число A"))
-let B = Number(prompt("введите число B"))
-if (A > B) {
-    let X = A*B;
-    alert("X="+X);
+let a = Number(prompt("введите число A"))
+let b = Number(prompt("введите число B"))
+if (a > b) {
+    let x = a*b;
+    alert("x="+x);
 } else {
-    let X = A+B;
-    alert("X="+X);
+    let x = a+b;
+    alert("x="+x);
 }
 
 
 // Задание № 2 Калькулятор возраста
 
-let A = Number(prompt("введите год рождения"))
-let B = 2025
-let X = B-A
-alert(`Ваш возраст ${X} лет`)
+let a = Number(prompt("введите год рождения"))
+let b = 2025
+let x = b-a
+alert(`Ваш возраст ${x} лет`)
 
 
 // Задание № 3 Определение максимального числа из трех введенных
 
-let A = Number(prompt("введите число A"))
-let B = Number(prompt("введите число B"))
-let C = Number(prompt("введите число C"))
-if (A > B) {
-    alert(`Максимальное число ${A}`);
-} else if (A > C) {
-    alert(`Максимальное число ${A}`);
-} else if (B > A) {
-    alert(`Максимальное число ${B}`);
-} else if (B > C) {
-    alert(`Максимальное число ${B}`);
+let a = Number(prompt("введите число A"))
+let b = Number(prompt("введите число B"))
+let c = Number(prompt("введите число C"))
+if (a > b) {
+    alert(`Максимальное число ${a}`);
+} else if (a > c) {
+    alert(`Максимальное число ${a}`);
+} else if (b > a) {
+    alert(`Максимальное число ${b}`);
+} else if (b > c) {
+    alert(`Максимальное число ${b}`);
 } else {
-    alert(`Максимальное число ${C}`);
+    alert(`Максимальное число ${c}`);
+}
+
+
+// Задание № 4 Проверка пароля
+
+let kod = prompt("Введите пароль")
+
+if (kod == 123321) {
+    alert("Доступ разрешен");
+} else {
+    alert("Ошибка");
+}
+
+// Задание № 5 Конвертер температуры
+
+
 }
